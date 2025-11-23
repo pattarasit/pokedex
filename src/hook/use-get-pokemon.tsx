e7d1e37fb8dee7cf1b656e3585dc39742423d5db
Pattarasit Lomthaisong <57983249+pattarasit@users.noreply.github.com>
2025-11-23 20:06:34 +0700

    init router api

---
+++ src/hook/use-get-pokemon.tsx
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+
+export const useGetAllPokemon = () => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['pokemon-all'],
+    queryFn: () => fetch('https://pokeapi.co/api/v2/pokemon').then(res => res.json()),
+  });
+
+  return { data, isLoading, error };
+};  
+
+
+export const useGetPokemon = (url: string | undefined) => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['pokemon-', url],
+    queryFn: () => fetch(url || '').then(res => res.json()),
+    enabled: !!url,
+  });
+
+  return { data, isLoading, error };
+};
--- src/page/pokedex/index.tsx
+++ src/page/pokedex/index.tsx
@@ -1,5 +1,32 @@
+import { Button } from "@/components/ui/button";
+import { useGetAllPokemon, useGetPokemon } from "../../hook/use-get-pokemon";
+import { Pokemon } from "../../types/pokemon";
+import { useState } from "react";
+
 export const Pokedex = () => {
+  const [selectedPokemon, setSelectedPokemon] = useState<string | undefined>(undefined);
+  const {data} = useGetAllPokemon();
+  const {data: pokemon} = useGetPokemon(selectedPokemon);
+  console.log('dpokemonata => ', pokemon);
+
   return (
-    <div>pokedex</div>
+    <div>
+      <h1>Pokedex</h1>
+      <div className="grid grid-cols-4 gap-4">
+        {data?.results.map((pokemon: Pokemon) => (
+          <Button onClick={() => setSelectedPokemon(pokemon.url)} key={pokemon.name}>{pokemon.name}</Button>
+        ))}
+      </div>
+
+
+<div>
+  {pokemon && (
+    <div>
+      <h2>{pokemon.name}</h2>
+      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+    </div>
+  )}
+</div>
+    </div>
   )
 }
